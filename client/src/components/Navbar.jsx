a76030662468c3856eca241b6b01cde6bc057878
balint-szabox <70958880+balint-szabox@users.noreply.github.com>
2023-04-21 16:26:17 +0200

    navbar created and setup react router

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,119 @@
+import {
+  Box,
+  Flex,
+  HStack,
+  Link,
+  IconButton,
+  Icon,
+  Text,
+  useDisclosure,
+  Button,
+  Stack,
+  useColorModeValue,
+  useColorMode,
+} from "@chakra-ui/react";
+import { Link as ReactLink } from "react-router-dom";
+import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { BsScissors } from "react-icons/bs";
+
+const links = [
+  { linkName: "Products", path: "/products" },
+  { linkName: "ShoppingCart", path: "/cart" },
+];
+
+const NavLink = ({ path, children }) => (
+  <Link
+    as={ReactLink}
+    to={path}
+    px={2}
+    py={2}
+    rounded={"md"}
+    _hover={{
+      textDecoration: "none",
+      bg: useColorModeValue("gray.200", "gray.700"),
+    }}
+  >
+    {children}
+  </Link>
+);
+
+const Navbar = () => {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+      <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+        <IconButton
+          size={"md"}
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          display={{ md: "none" }}
+          onClick={isOpen ? onClose : onOpen}
+        ></IconButton>
+        <HStack>
+          <Link as={ReactLink} to="/">
+            <Flex alignItems="center">
+              <Icon as={BsScissors} h={6} w={6} color={"purple.400"}></Icon>
+              <Text fontWeight={"extrabold"}>Józsa Adri Hair</Text>
+            </Flex>
+          </Link>
+          <HStack as={"nav"} spacing={4} display={{ base: "none", md: "flex" }}>
+            {links.map((link) => (
+              <NavLink key={link.linkName} path={link.path}>
+                {link.linkName}
+              </NavLink>
+            ))}
+          </HStack>
+        </HStack>
+        <Flex align={"center"}>
+          <NavLink>
+            <Icon
+              as={colorMode === "light" ? MoonIcon : SunIcon}
+              alignSelf="center"
+              onClick={() => toggleColorMode()}
+            />
+          </NavLink>
+          <Button
+            as={ReactLink}
+            to={"/login"}
+            p={2}
+            fontSize={"sm"}
+            fontWeight={400}
+            variant={"link"}
+          >
+            Sign In
+          </Button>
+          <Button
+            as={ReactLink}
+            to={"/registration"}
+            m={2}
+            display={{ base: "none", md: "inline-flex" }}
+            fontSize={"sm"}
+            p={2}
+            fontWeight={600}
+            _hover={{ bg: "purple.400" }}
+            bg={"purple.500"}
+            color={"white"}
+          >
+            Sign Up
+          </Button>
+        </Flex>
+      </Flex>
+      {isOpen ? (
+        <Box pb={4} display={{ md: "none" }}>
+          <Stack as={"nav"} spacing={4}>
+            {links.map((link) => (
+              <NavLink key={link.linkName} path={link.path}>
+                {link.linkName}
+              </NavLink>
+            ))}
+            <NavLink key={"sing up"} path={"/regestration"}>
+              Sign Up
+            </NavLink>
+          </Stack>
+        </Box>
+      ) : null}
+    </Box>
+  );
+};
+
+export default Navbar;
